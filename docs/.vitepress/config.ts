efcba1bb499833dfa8a9a87a66901dfc1d635be0
baboribo <inertia_mailer_0c@icloud.com>
2025-09-09 00:24:29 +0900

    ci: 문제 해결을 위해 base를 대소문자를 지킨 repo로 수정함.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -129,7 +129,7 @@ export default defineConfig({
   },
 
   // ⚙️ 빌드 설정
-  base: '/playbot-docs/', // GitHub Pages라면 '/repo-name/'로 변경
+  base: '/PlayBot-Docs/', // GitHub Pages라면 '/repo-name/'로 변경
 
   // 🛠 Markdown 설정 (선택 사항)
   markdown: {
